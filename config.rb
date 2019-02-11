cb76977b641aeea1de80a1b84cb51b639606a55d
takuyan <info@takuyan.com>
2019-02-11 21:02:38 +0900

    use middleman-livereload

--- config.rb
+++ config.rb
@@ -106,7 +106,7 @@ activate :directory_indexes
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
